17b908563e95c710deaf94d253f71a0657d93565
phabets <phabets@student.42.fr>
2023-02-20 10:32:53 +0100

    oiui

--- src/raycastingtest.c
+++ src/raycastingtest.c
@@ -1,22 +1,31 @@
 #include "../mlx/mlx.h"
 #include <math.h>
 
-int worldMap[8][8]= 
+int worldMap[16][16]= 
 {
-	{1,3,2,3,2,3,2,1},
-	{2,0,0,0,0,0,0,3},
-	{1,0,3,0,0,0,0,3},
-	{2,0,0,0,0,0,0,3},
-	{1,0,0,0,0,0,0,3},
-	{2,0,0,0,0,3,0,3},
-	{2,0,0,0,0,0,0,3},
-	{1,3,1,3,1,3,1,1}
+	{1,3,2,3,2,3,2,1,2,3,2,3,2,3,2,1},
+	{2,0,1,0,0,0,0,0,0,0,0,0,1,0,0,3},
+	{1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1},
+	{2,0,0,0,1,0,1,0,0,0,1,0,0,0,0,3},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,1,1,1,1,1,1,0,0,3},
+	{1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,1,0,1,1,1,1,0,0,3},
+	{1,0,0,0,3,0,0,2,0,2,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,2,0,2,0,0,0,0,0,3},
+	{2,0,0,0,0,0,0,2,0,2,0,0,0,0,0,3},
+	{1,3,1,3,1,3,1,3,1,3,1,3,1,3,1,1}
 };
 
 typedef struct s_game
 {
 	void	*mlx;
 	void	*mlx_win;
+	void	*win;
 
 	double posX;
 	double posY;
@@ -47,22 +56,9 @@ void	verLine(t_game *info, int x, int y1, int y2, int color)
 	}
 }
 
-int	main(void)
+
+void	render_frame(t_game game)
 {
-	t_game game;
-
-	game.posX = 5;
-	game.posY = 4;
-	game.dirX = -1;
-	game.dirY = 0;
-	game.planeX = 0;
-	game.planeY = 0.66;
-
-	game.mlx = mlx_init();
-	game.mlx_win = mlx_new_window(game.mlx, 640, 480, "Cub3d");
-	while(1)
-	{
-		sleep(1);
 		mlx_clear_window(game.mlx, game.mlx_win);
 		for(int x = 0; x < 640; x++)
 		{
@@ -112,7 +108,6 @@ int	main(void)
 					game.mapY += stepY;
 					side = 1;
 				}
-				printf("mapX :%i, mapY :%i.\n",game.mapX,game.mapY);
 				if(worldMap[game.mapX][game.mapY] > 0)
 					hit = 1;
 			}
@@ -137,14 +132,84 @@ int	main(void)
 
 			verLine(&game, x, drawStart, drawEnd, color);
 		}
+
+}
+
+int	player_move(int keycode, t_game *game)
+{
+	if (keycode == 65307)
+		exit(0);
+	else if (keycode == 119)//forward W
+	{
+		game->posX += (game->dirX / 10);
+		game->posY += (game->dirY / 10);
+		render_frame(*game);
+	}
+	else if (keycode == 100)//rotate right E
+	{
+		double oldDirX = game->dirX;
+		game->dirX = game->dirX * cos(50) - game->dirY * sin(50);
+		game->dirY = oldDirX * sin(50) + game->dirY * cos(50);
+		double oldPlaneX = game->planeX;
+		game->planeX = game->planeX * cos(50) - game->planeY * sin(50);
+		game->planeY = oldPlaneX * sin(50) + game->planeY * cos(50);
+		render_frame(*game);
+	}
+
+	else if (keycode == 115)//backward S
+	{
+		game->posX -= game->dirX;
+		game->posY -= game->dirY;
+		render_frame(*game);
+	}
+	else if (keycode == 101)//right D
+	{
+		game->posX += game->dirX;
+    	game->posY += game->dirY;
+		render_frame(*game);
+	}
+	else if (keycode == 97)//left A
+	{
+		double oldDirX = game->dirX;
+		game->dirX = game->dirX * cos(-50) - game->dirY * sin(-50);
+		game->dirY = oldDirX * sin(-50) + game->dirY * cos(-50);
+		double oldPlaneX = game->planeX;
+		game->planeX = game->planeX * cos(-50) - game->planeY * sin(-50);
+		game->planeY = oldPlaneX * sin(-50) + game->planeY * cos(-50);
+		render_frame(*game);
+	}
+	else
+		printf("%i\n", keycode);
+
+	return (0);
+}
+
+int	main(void)
+{
+	t_game game;
+
+	game.posX = 7;
+	game.posY = 7;
+	game.dirX = -1;
+	game.dirY = 0;
+	game.planeX = 0;
+	game.planeY = 0.66;
+
+	game.mlx = mlx_init();
+	game.mlx_win = mlx_new_window(game.mlx, 640, 480, "Cub3d");
+	//while(1)
+	//{
+		/*
 		double oldDirX = game.dirX;
 		game.dirX = game.dirX * cos(-100) - game.dirY * sin(-100);
 		game.dirY = oldDirX * sin(-100) + game.dirY * cos(-100);
 		double oldPlaneX = game.planeX;
 		game.planeX = game.planeX * cos(-100) - game.planeY * sin(-100);
-		game.planeY = oldPlaneX * sin(-100) + game.planeY * cos(-100);
-	}
+		game.planeY = oldPlaneX * sin(-100) + game.planeY * cos(-100);*/
+//	}
 //	mlx_loop_hook(game.mlx, raycasting, game);
+	render_frame(game);
+	mlx_hook(game.mlx_win, 2, 1L<<0, player_move, &game);
 	mlx_loop(game.mlx);
 	return (0);
 }
